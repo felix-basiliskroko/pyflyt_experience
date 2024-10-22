d74560d41aa0eda909d4d6ba650c310778af96de
Felix Unterleiter <80671362+felix-basiliskroko@users.noreply.github.com>
2024-10-22 14:10:19 +0200

    PPO uses custom net arch
    network architecture as defined in paper (https://arxiv.org/pdf/2111.07037) to see if this shit becomes better

--- train_test.py
+++ train_test.py
@@ -1,4 +1,5 @@
 import datetime
+import torch as t
 import gymnasium as gym
 from gymnasium.wrappers import RecordVideo
 import PyFlyt.gym_envs
@@ -11,7 +12,7 @@ from stable_baselines3.common.env_util import make_vec_env
 from custom_callbacks import ObservationHistCallback
 
 # Logdir
-dir = "./tensorboard_log/StaticWaypointEnv/SimpleObs"
+dir = "./tensorboard_log/StaticWaypointEnv/Simple-ObsCustomNet"
 # hist_callback = ObservationHistCallback()
 # Get current date and time
 current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -23,6 +24,7 @@ vec_env = make_vec_env("Quadx-Waypoint-v0", n_envs=1)
 # vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=True)
 
 # model = PPO("MlpPolicy", vec_env, verbose=1, tensorboard_log=dir)  # For non-dict observation space
-model = PPO("MultiInputPolicy", vec_env, verbose=1, tensorboard_log=dir)  # For non-dict observation space
+policy_kwargs = dict(activation_fn=t.nn.Tanh, net_arch=dict(pi=[64, 64], vf=[64, 64]))
+model = PPO("MultiInputPolicy", vec_env, verbose=0, tensorboard_log=dir, policy_kwargs=policy_kwargs)  # For non-dict observation space
 model.learn(total_timesteps=500_000)
 model.save("ppo_waypoint")
