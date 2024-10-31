6f1033d5ec38a48d01a034a28bea1bd0b31fcfb8
Felix Unterleiter <80671362+felix-basiliskroko@users.noreply.github.com>
2024-10-31 16:12:44 +0100

    Implement LOS-Observation Space
    as proposed in paper, let's see how this performs

--- Envs/static_waypoint.py
+++ Envs/static_waypoint.py
@@ -1,74 +1,207 @@
-from PyFlyt.gym_envs.quadx_envs.quadx_waypoints_env import QuadXWaypointsEnv
-import gymnasium
+from __future__ import annotations
+
+from typing import Any, Literal
+
 import numpy as np
-from PyFlyt.gym_envs.utils.waypoint_handler import WaypointHandler
 from gymnasium import spaces
 
-class SingleWaypointQuadXEnv(QuadXWaypointsEnv):
-    def __init__(self, **kwargs):
-        # Force only one waypoint in the environment
-        super().__init__(num_targets=1, sparse_reward=True, **kwargs)
+from PyFlyt.gym_envs.quadx_envs.quadx_base_env import QuadXBaseEnv
+from PyFlyt.gym_envs.utils.waypoint_handler import WaypointHandler
 
-        # the whole implicit state space = attitude + previous action + auxiliary information
-        self.combined_space = spaces.Box(
-            low=-np.inf,
-            high=np.inf,
-            shape=(
-                10  # not 12 because we do not consider linear position
-                + self.action_space.shape[0]
-                + self.auxiliary_space.shape[0],
-            ),
-            dtype=np.float64,
+
+class SingleWaypointQuadXEnv(QuadXBaseEnv):
+    """QuadX Waypoints Environment.
+
+    Actions are vp, vq, vr, T, ie: angular rates and thrust.
+    The target is a set of `[x, y, z, (optional) yaw]` waypoints in space.
+
+    Args:
+    ----
+        sparse_reward (bool): whether to use sparse rewards or not.
+        num_targets (int): number of waypoints in the environment.
+        use_yaw_targets (bool): whether to match yaw targets before a waypoint is considered reached.
+        goal_reach_distance (float): distance to the waypoints for it to be considered reached.
+        goal_reach_angle (float): angle in radians to the waypoints for it to be considered reached, only in effect if `use_yaw_targets` is used.
+        flight_mode (int): the flight mode of the UAV.
+        flight_dome_size (float): size of the allowable flying area.
+        max_duration_seconds (float): maximum simulation time of the environment.
+        angle_representation (Literal["euler", "quaternion"]): can be "euler" or "quaternion".
+        agent_hz (int): looprate of the agent to environment interaction.
+        render_mode (None | Literal["human", "rgb_array"]): render_mode
+        render_resolution (tuple[int, int]): render_resolution.
+
+    """
+
+    def __init__(
+            self,
+            num_targets: int = 1,
+            use_yaw_targets: bool = False,
+            goal_reach_distance: float = 0.2,
+            goal_reach_angle: float = 0.1,
+            flight_mode: int = 0,
+            flight_dome_size: float = 50.0,
+            max_duration_seconds: float = 10.0,
+            angle_representation: Literal["euler", "quaternion"] = "quaternion",
+            agent_hz: int = 30,
+            render_mode: None | Literal["human", "rgb_array"] = None,
+            render_resolution: tuple[int, int] = (480, 480),
+    ):
+        """__init__.
+
+        Args:
+        ----
+            sparse_reward (bool): whether to use sparse rewards or not.
+            num_targets (int): number of waypoints in the environment.
+            use_yaw_targets (bool): whether to match yaw targets before a waypoint is considered reached.
+            goal_reach_distance (float): distance to the waypoints for it to be considered reached.
+            goal_reach_angle (float): angle in radians to the waypoints for it to be considered reached, only in effect if `use_yaw_targets` is used.
+            flight_mode (int): the flight mode of the UAV.
+            flight_dome_size (float): size of the allowable flying area.
+            max_duration_seconds (float): maximum simulation time of the environment.
+            angle_representation (Literal["euler", "quaternion"]): can be "euler" or "quaternion".
+            agent_hz (int): looprate of the agent to environment interaction.
+            render_mode (None | Literal["human", "rgb_array"]): render_mode
+            render_resolution (tuple[int, int]): render_resolution.
+
+        """
+        super().__init__(
+            start_pos=np.array([[0.0, 0.0, 1.0]]),
+            flight_mode=flight_mode,
+            flight_dome_size=flight_dome_size,
+            max_duration_seconds=max_duration_seconds,
+            angle_representation=angle_representation,
+            agent_hz=agent_hz,
+            render_mode=render_mode,
+            render_resolution=render_resolution,
         )
 
-        # Redefine the observation space to remove the sequence observation
-        self.observation_space = gymnasium.spaces.Dict({
-            "attitude": self.combined_space,
-            "target_deltas": gymnasium.spaces.Box(
-                low=-2 * self.flight_dome_size,
-                high=2 * self.flight_dome_size,
-                shape=(3,),
-                dtype=np.float64,
-            ),
-            "altitude": gymnasium.spaces.Box(
-                low=0.0,
-                high=1.0,
-                shape=(1,),
-                dtype=np.float64,
-            )
-        })
-
         # define waypoints
-        self.waypoints.flight_dome_size = 10.0
-        self.waypoints.goal_reach_distance = 0.5
-        self.waypoints.enable_render = True
-        self.waypoints.min_height = 5
+        self.state = None
+        self.reward_info = {
+            "scaled_los_reward": 0.0,
+            "scaled_altitude_reward": 0.0,
+            "scaled_smooth_reward": 0.0,
+        }
 
-    def compute_state(self):
+        self.waypoints = WaypointHandler(
+            enable_render=self.render_mode is not None,
+            num_targets=num_targets,
+            use_yaw_targets=use_yaw_targets,
+            goal_reach_distance=goal_reach_distance,
+            goal_reach_angle=goal_reach_angle,
+            flight_dome_size=flight_dome_size,
+            min_height=0.1,
+            np_random=self.np_random,
+        )
+
+        # Define observation space
+        self.observation_space = spaces.Dict(
+            {
+                "t_azimuth_angle": spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float64),
+                "t_elevation_angle": spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float64),
+                "a_azimuth_angle": spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float64),
+                "a_elevation_angle": spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float64),
+                "aux_state": spaces.Box(low=-np.inf, high=np.inf, shape=(4,), dtype=np.float64),
+                "altitude": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float64),
+            })
+
+    def reset(
+            self, *, seed: None | int = None, options: None | dict[str, Any] = dict()
+    ):
+        """Resets the environment.
+
+        Args:
+        ----
+            seed: seed to pass to the base environment.
+            options: None
+
+        """
+        super().begin_reset(seed, options)
+        self.waypoints.reset(self.env, self.np_random)
+        self.info["num_targets_reached"] = 0
+        super().end_reset()
+
+        self.state = {
+            "t_azimuth_angle": np.zeros(1),
+            "t_elevation_angle": np.zeros(1),
+            "a_azimuth_angle": np.zeros(1),
+            "a_elevation_angle": np.zeros(1),
+            "aux_state": np.zeros(4),
+            "altitude": np.zeros(1),
+        }
+
+        return self.state, self.info
+
+    def compute_state(self) -> None:
         """Computes the state for a single waypoint environment."""
-        super().compute_state()
+        # super().compute_state()
         # Since there's only one waypoint, we take the first and only target delta
         if self.angle_representation == 1:
-            ang_vel, ang_pos, lin_vel, lin_pos, quaternion = super().compute_attitude()
-            altitude = lin_pos[2] if lin_pos[2] < 1.0 else 1.0
-            self.state["attitude"] = np.concatenate([
-                ang_vel, quaternion, lin_vel, self.action, super().compute_auxiliary()
-            ], axis=-1)
-            self.state["target_deltas"] = self.waypoints.distance_to_targets(ang_pos=ang_pos, lin_pos=lin_pos, quaternion=quaternion)[0]
-            self.state["altitude"] = np.array([altitude])
+            _, _, lin_vel, lin_pos, _ = super().compute_attitude()
+
+            t_xy_proj, a_xy_proj = self.waypoints.targets[0][:2], lin_vel[:2]
+            t_az_ang = np.arctan2(t_xy_proj[1], t_xy_proj[0])
+            a_az_ang = np.arctan2(a_xy_proj[1], a_xy_proj[0])
+
+            t_xz_proj, a_xz_proj = self.waypoints.targets[0][[0, 2]], lin_vel[[0, 2]]
+            t_el_ang = np.arctan2(t_xz_proj[1], t_xz_proj[0])
+            a_el_ang = np.arctan2(a_xz_proj[1], a_xz_proj[0])
+
+            new_state = dict()
+            new_state["t_azimuth_angle"] = np.array([t_az_ang])
+            new_state["t_elevation_angle"] = np.array([t_el_ang])
+            new_state["a_azimuth_angle"] = np.array([a_az_ang])
+            new_state["a_elevation_angle"] = np.array([a_el_ang])
+            new_state["aux_state"] = super().compute_auxiliary()
+            new_state["altitude"] = np.array([lin_pos[2]]) if lin_pos[2] < 1.0 else np.array([1.0])
         else:
             raise NotImplementedError("Only quaternion representation is supported for now.")
 
-    def reset(self, *, seed=None, options=None):
-        """Reset the environment with only one waypoint."""
-        return super().reset(seed=seed, options=options)
+        self.state = new_state
 
     def compute_term_trunc_reward(self):
         """Handle termination, truncation, and reward specifically for single waypoint."""
-        super().compute_term_trunc_reward()
-        distance_reward = -np.linalg.norm(self.state["target_deltas"])  # Negative distance to target as reward
-        altitude_reward = -1/self.state["altitude"]  # Negative altitude as reward
-        self.reward = distance_reward + altitude_reward
+        if np.any(self.env.contact_array[self.env.planeId]):
+            self.reward = -100.0
+            self.info["collision"] = True
+            self.termination |= True
+
+        # exceed flight dome
+        if np.linalg.norm(self.env.state(0)[-1]) > self.flight_dome_size:
+            self.reward = -100.0
+            self.info["out_of_bounds"] = True
+            self.termination |= True
+
+        # target reached
+        if self.waypoints.target_reached:
+            self.reward = 100.0
+
+            # advance the targets
+            self.waypoints.advance_targets()
+
+            # update infos and dones
+            self.truncation |= self.waypoints.all_targets_reached
+            self.info["env_complete"] = self.waypoints.all_targets_reached
+            self.info["num_targets_reached"] = self.waypoints.num_targets_reached
+
+        los_reward = ((np.pi/2 - np.abs(self.state["t_azimuth_angle"] - self.state["a_azimuth_angle"])) + (np.pi/2 - np.abs(self.state["t_elevation_angle"] - self.state["a_elevation_angle"]))) - np.pi
+        # -2*pi if the agent is facing the opposite direction of the target; 0 if the agent is perfectly aligned with the target
+        altitude_reward = self.state["altitude"]  # Negative altitude as reward
+        smooth_reward = -np.linalg.norm(self.state["aux_state"] - self.action)  # Negative smooth control reward
+
+        # Min-Max scaling to ensure all rewards are in the range [-2*pi, 0]
+        scaled_los_reward = los_reward
 
 
+        scaled_altitude_reward = (altitude_reward * 2*np.pi) - 2*np.pi
 
+        smooth_reward_min = -np.linalg.norm(np.array([2*np.pi, 2*np.pi, 2*np.pi, 2*np.pi, 0.8]))
+        smooth_reward_max = 0.0
+        scaled_smooth_reward = (((smooth_reward - smooth_reward_min) / (smooth_reward_max - smooth_reward_min)) * 2*np.pi) - 2*np.pi
+
+        self.reward_info["scaled_los_reward"] = scaled_los_reward
+        self.reward_info["scaled_altitude_reward"] = scaled_altitude_reward
+        self.reward_info["scaled_smooth_reward"] = scaled_smooth_reward
+
+        reward = 0.2 * scaled_los_reward + 0.6 * scaled_altitude_reward + 0.2 * scaled_smooth_reward
+        self.reward = reward[0]
--- Envs/test_WayPointEnv.py
+++ Envs/test_WayPointEnv.py
@@ -11,7 +11,7 @@ from WaypointEnv import QuadXWaypoint
 # id = "QuadX-Waypoint-v0"
 id = "SingleWaypointQuadXEnv-v0"
 
-env = gymnasium.make(id, render_mode="human")
+env = gymnasium.make(id, render_mode=None)
 # env = gymnasium.make("Quadx-Waypoint-v0", render_mode=None)
 
 term, trunc = False, False
@@ -19,9 +19,15 @@ obs, _ = env.reset()
 print(f'Current waypoint: {env.unwrapped.waypoints.targets[0]}')
 
 while not (term or trunc):
-    new_action = env.action_space.sample()
+    # new_action = env.action_space.sample()
+    new_action = np.array([0.0, 0.0, 0.0, 0.0])
     # new_action = np.array([0.8, 0.8, 0.8, 0.8])
     obs, rew, term, trunc, _ = env.step(new_action)
+    reward_info = env.reward_info
+    print(f'Current altitude: {obs["altitude"]} ("scaled_altitude_reward": {reward_info["scaled_altitude_reward"]})')
+    print(f'Action - aux_state: {new_action - obs["aux_state"]} - its L2 norm: {np.linalg.norm(new_action - obs["aux_state"])}; ("scaled_smooth_reward": {reward_info["scaled_smooth_reward"]})')
+    print(f'Reward: {rew}')
+
 
 env.reset()
 
