85de09112e4f1179492991f44ea5ee999fc9d148
Felix Unterleiter <80671362+felix-basiliskroko@users.noreply.github.com>
2024-11-16 18:36:44 +0100

    gamma tuner

--- train/hyperparameters/ppo_tuning.py
+++ train/hyperparameters/ppo_tuning.py
@@ -17,13 +17,37 @@ def entr_tune(env_id: str, log_dir: str, value_range: list, buckets: int, num_st
     ent_coeff_vals = np.linspace(value_range[0], value_range[1], buckets)
 
     for ent_coeff in ent_coeff_vals:
-        run_dir = f"{log_dir}/ent_coeff={ent_coeff}"
+        run_dir = f"{log_dir}/ent_coeff={round(ent_coeff, 3)}"
         model = PPO("MultiInputPolicy",
                     vec_env,
                     verbose=0,
                     tensorboard_log=run_dir,
                     policy_kwargs=policy_kwargs,
                     ent_coef=ent_coeff,
+                    device=device)
+        model.learn(total_timesteps=num_steps)
+    print("Finished tuning entropy coefficient.")
+
+
+def gamma_tune(env_id: str, log_dir: str, value_range: list, buckets: int, num_steps: int):
+    device = "cuda" if t.cuda.is_available() else "cpu"
+    vec_env = make_vec_env(env_id=env_id, n_envs=1, seed=69)
+    policy_kwargs = dict(activation_fn=t.nn.Tanh, net_arch=dict(pi=[64, 64], vf=[64, 64]))
+    log_dir = os.path.join(log_dir, env_id, "gamma")
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir, exist_ok=True)
+    log_dir = f"{log_dir}/v_range={value_range}_buckets={buckets}_steps={num_steps}"
+    gamma_vals = np.linspace(value_range[0], value_range[1], buckets)
+
+    for gamma in gamma_vals:
+        run_dir = f"{log_dir}/gamma={round(gamma, 3)}"
+        model = PPO("MultiInputPolicy",
+                    vec_env,
+                    verbose=0,
+                    tensorboard_log=run_dir,
+                    policy_kwargs=policy_kwargs,
+                    ent_coef=0.007,
+                    gamma=gamma,
                     device=device)
         model.learn(total_timesteps=num_steps)
     print("Finished tuning entropy coefficient.")
